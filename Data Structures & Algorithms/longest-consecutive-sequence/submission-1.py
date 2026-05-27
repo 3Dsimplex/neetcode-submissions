7c8bb5546b31d24e5e32af62fe0d316ce00a1657
3Dsimplex <89542561+3Dsimplex@users.noreply.github.com>
2026-05-28 01:57:18 +0530

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        size = [None for _ in nums]
+
+        def calc_size(ind: int):
+            print(ind)
+            if size[ind]: return size[ind]
+            if ind == len(nums):
+                size[ind] = 1
+                return 1
+            max_size = 0
+            for j in range(len(nums)):
+                if nums[j] == nums[ind] + 1: 
+                    max_size = max(max_size, calc_size(j))
+            size[ind] = max_size + 1
+            return max_size + 1
+
+    
+        for i in range(len(nums)): calc_size(i)
+        if size: return max(size)
+        else: return 0
+        
