fd3ab2c2fbc0267948d370a10342794861655b28
3Dsimplex <89542561+3Dsimplex@users.noreply.github.com>
2026-05-28 00:39:52 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counts = [0 for i in range(2001)]
+        for num in nums: counts[num] += 1 
+        ans = []
+        while len(ans) < k:
+            i = 0
+            for num in nums:
+                if (num) in ans: continue
+                if (i) in ans: i = num
+                if counts[num] > counts[i]: i = num
+            ans.append(i)
+        return ans
