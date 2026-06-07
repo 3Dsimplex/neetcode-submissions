eeffe65a68c21a07e139e9972d234a4fe1b8db50
3Dsimplex <89542561+3Dsimplex@users.noreply.github.com>
2026-06-08 04:20:04 +0530

    Add: subtree-of-a-binary-tree - submission-6

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-6.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isEqual(TreeNode* tree1, TreeNode* tree2) {
+        if (tree1 == tree2) return true;
+        if (!(tree1 && tree2)) return false;
+
+        return (tree1->val == tree2->val) && isEqual(tree1->left, tree2->left) &&
+                isEqual(tree1->right, tree2->right);
+
+}    
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!subRoot) return true;
+        if (!root) return false;
+
+        return isEqual(root, subRoot) ||
+            isSubtree(root->left, subRoot) ||
+            isSubtree(root->right, subRoot);
+    }
+};
